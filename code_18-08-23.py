c2a1ec18fbd93dc6507116bdf66e19d20190a8e5
Anubhav Bisht <40894333+anubhavbisht@users.noreply.github.com>
2023-08-18 10:34:37 +0530

    wip

--- code_18-08-23.py
+++ code_18-08-23.py
@@ -1,1 +1,9 @@
-print('Hi')
+# This program says Hello and asks for your name and age
+print('Hello World')
+print('What is your name')
+myName = input()
+print('Hello',myName)
+print('Your name is',len(myName),'characters long')
+print('What is your age?')
+age = input()
+print('Hello',myName,',you will be',str(int(age)+1),'years old next year') 
